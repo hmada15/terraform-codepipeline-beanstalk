0fb58cafbbd52fe7bc3439019633601b0d463305
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-07-22 14:10:06 +0200

    move iam policy to the data file with a better syntax

---
+++ data.tf
@@ -0,0 +1,178 @@
+data "aws_iam_policy_document" "codepipeline-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "codepipeline-policy" {
+  statement {
+    actions   = ["iam:PassRole"]
+    resources = ["*"]
+    condition {
+      test     = "StringEqualsIfExists"
+      variable = "iam:PassedToService"
+      values = [
+        "cloudformation.amazonaws.com",
+        "elasticbeanstalk.amazonaws.com",
+        "ec2.amazonaws.com",
+        "ecs-tasks.amazonaws.com"
+      ]
+    }
+  }
+
+  statement {
+    actions = [
+      "codecommit:CancelUploadArchive",
+      "codecommit:GetBranch",
+      "codecommit:GetCommit",
+      "codecommit:GetRepository",
+      "codecommit:GetUploadArchiveStatus",
+      "codecommit:UploadArchive"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "codedeploy:CreateDeployment",
+      "codedeploy:GetApplication",
+      "codedeploy:GetApplicationRevision",
+      "codedeploy:GetDeployment",
+      "codedeploy:GetDeploymentConfig",
+      "codedeploy:RegisterApplicationRevision"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "codestar-connections:UseConnection"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "elasticbeanstalk:*",
+      "ec2:*",
+      "elasticloadbalancing:*",
+      "autoscaling:*",
+      "cloudwatch:*",
+      "s3:*",
+      "sns:*",
+      "cloudformation:*",
+      "rds:*",
+      "sqs:*",
+      "ecs:*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "lambda:InvokeFunction",
+      "lambda:ListFunctions"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "opsworks:CreateDeployment",
+      "opsworks:DescribeApps",
+      "opsworks:DescribeCommands",
+      "opsworks:DescribeDeployments",
+      "opsworks:DescribeInstances",
+      "opsworks:DescribeStacks",
+      "opsworks:UpdateApp",
+      "opsworks:UpdateStack"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "cloudformation:CreateStack",
+      "cloudformation:DeleteStack",
+      "cloudformation:DescribeStacks",
+      "cloudformation:UpdateStack",
+      "cloudformation:CreateChangeSet",
+      "cloudformation:DeleteChangeSet",
+      "cloudformation:DescribeChangeSet",
+      "cloudformation:ExecuteChangeSet",
+      "cloudformation:SetStackPolicy",
+      "cloudformation:ValidateTemplate"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+      "codebuild:BatchGetBuildBatches",
+      "codebuild:StartBuildBatch"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "devicefarm:ListProjects",
+      "devicefarm:ListDevicePools",
+      "devicefarm:GetRun",
+      "devicefarm:GetUpload",
+      "devicefarm:CreateUpload",
+      "devicefarm:ScheduleRun"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "servicecatalog:ListProvisioningArtifacts",
+      "servicecatalog:CreateProvisioningArtifact",
+      "servicecatalog:DescribeProvisioningArtifact",
+      "servicecatalog:DeleteProvisioningArtifact",
+      "servicecatalog:UpdateProduct"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "cloudformation:ValidateTemplate"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "ecr:DescribeImages"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "states:DescribeExecution",
+      "states:DescribeStateMachine",
+      "states:StartExecution"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "appconfig:StartDeployment",
+      "appconfig:StopDeployment",
+      "appconfig:GetDeployment"
+    ]
+    resources = ["*"]
+  }
+}
